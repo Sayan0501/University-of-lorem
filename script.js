f0c99ea6308d9ca7e79546ec7522d65d0278339c
Sayan Sarkar <111379709+Sayan0501@users.noreply.github.com>
2024-03-09 14:33:15 +0530

    updated file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function openMenu(){
-    var navlist = document.getElementById("navlist");
+    var nav_ul_list = document.getElementById("nav_ul_list");
     var nav__Toggle = document.querySelector(".nav__toggle");
-    navlist.classList.toggle("active");
+    nav_ul_list.classList.toggle("active");
     nav__Toggle.classList.toggle("open");
 }
