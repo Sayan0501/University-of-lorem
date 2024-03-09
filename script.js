4c2c57f3d2ad8f6ff10a2b0debc71d9b7b1c0411
Sayan Sarkar <111379709+Sayan0501@users.noreply.github.com>
2024-03-09 13:46:59 +0530

    dummy website name university of lorem

---
+++ script.js
@@ -0,0 +1,6 @@
+function openMenu(){
+    var navlist = document.getElementById("navlist");
+    var nav__Toggle = document.querySelector(".nav__toggle");
+    navlist.classList.toggle("active");
+    nav__Toggle.classList.toggle("open");
+}
