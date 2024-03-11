88263f60740cb19149bc7798b007f8797870ee2a
Sayan Sarkar <111379709+Sayan0501@users.noreply.github.com>
2024-03-11 15:13:18 +0530

    updated flie

--- script.js
+++
@@ -1,6 +0,0 @@
-function openMenu(){
-    var nav_ul_list = document.getElementById("nav_ul_list");
-    var nav__Toggle = document.querySelector(".nav__toggle");
-    nav_ul_list.classList.toggle("active");
-    nav__Toggle.classList.toggle("open");
-}
